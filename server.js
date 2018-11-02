bad67c4069732cdba3f511322e9f07eebe9da3de
Kymberly Grove <kygrove12@gmail.com>
2018-11-01 22:17:37 -0700

    fixed test

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.status(200)});
-
 // let server;
 
 // function runServer() {
@@ -32,10 +29,10 @@ app.get('/', (req, res) => {
 //   });
 // }
 
-// if (require.main === module) {
-//   runServer().catch(err => console.error(err));
-// };
-
-app.listen(process.env.PORT || 8080);
+if (require.main === module) {
+  app.listen(process.env.PORT || 8080, function() {
+    console.info(`App listening on ${this.address().port}`);
+  });
+}
 
 module.exports = {app};
