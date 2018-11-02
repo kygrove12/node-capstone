a80849b52f8d9cac0199884f88e692f1110103ae
Kymberly Grove <kygrove12@gmail.com>
2018-11-01 21:36:42 -0700

    simplified test

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
 
 app.get('/', (req, res) => {
   res.status(200)});
 
-let server;
+// let server;
 
-function runServer() {
-  const port = process.env.PORT || 8080;
-  return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
-    });
-  });
-}
+// function runServer() {
+//   const port = process.env.PORT || 8080;
+//   return new Promise((resolve, reject) => {
+//     server = app.listen(port, () => {
+//       console.log(`Your app is listening on port ${port}`);
+//       resolve(server);
+//     }).on('error', err => {
+//       reject(err)
+//     });
+//   });
+// }
 
-function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
-    });
-  });
-}
+// function closeServer() {
+//   return new Promise((resolve, reject) => {
+//     console.log('Closing server');
+//     server.close(err => {
+//       if (err) {
+//         reject(err);
+//         return;
+//       }
+//       resolve();
+//     });
+//   });
+// }
 
-if (require.main === module) {
-  runServer().catch(err => console.error(err));
-};
+// if (require.main === module) {
+//   runServer().catch(err => console.error(err));
+// };
 
-module.export = {app, runServer, closeServer};
+app.listen(process.env.PORT || 8080);
+
+module.exports = {app};
--- test/test-server.js
+++ test/test-server.js
@@ -1,15 +1,15 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const {app, runServer, closeServer} = require("../server.js");
+const {app} = require("../server.js");
 const expect = chai.expect;
  chai.use(chaiHttp);
  describe("index page", function() {
- 	 before(function() {
-    return runServer();
-  });
- 	  after(function() {
-    return closeServer();
-  });
+ 	//  before(function() {
+  //   return runServer();
+  // });
+ 	//   after(function() {
+  //   return closeServer();
+  // });
  	  
   it("should exist", function() {
      return chai
